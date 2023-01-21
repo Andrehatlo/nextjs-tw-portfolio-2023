1aaa2931618d0194957357ef003afbf1f8f5c08c
André Hatlo-Johansen <ahatloj@gmail.com>
2023-01-21 20:03:59 +0100

    clean up

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -41,32 +41,32 @@ const Blog: React.FC<BlogProps> = ({posts}) => {
 }
 export default Blog;
 
-// export async function getStaticProps(){
-//   // Getting all our posts at build time
-//   // Get all the posts from posts folder
-//   const files = fs.readdirSync("posts");
-//   // Loop over each post to extract the frontmatter which we need
-//   const posts = files.map((file) => {
-//     // getting the slug here which we will need as a URL query parameter
-//     const slug = file.replace(".md", "");
-//     // Reading the contents of the file
-//     const filecontent = fs.readFileSync(`posts/${file}`, "utf-8");
-//     const parsedContent = matter(filecontent);
-//     //The parsed content contains data and content we only need the data which is the frontmatter
-//     const {data} = parsedContent
-//     return {
-//       slug,
-//       data,
-//     };
-//   });
-//     // sort posts by oldest to newest
-//     const sortedPosts = posts.sort(
-//       (firstEl, secondEl) => secondEl.data.date - firstEl.data.date
-//     ).reverse();
+export async function getStaticProps(){
+  // Getting all our posts at build time
+  // Get all the posts from posts folder
+  const files = fs.readdirSync("posts");
+  // Loop over each post to extract the frontmatter which we need
+  const posts = files.map((file) => {
+    // getting the slug here which we will need as a URL query parameter
+    const slug = file.replace(".md", "");
+    // Reading the contents of the file
+    const filecontent = fs.readFileSync(`./posts/${file}`, "utf-8");
+    const parsedContent = matter(filecontent);
+    //The parsed content contains data and content we only need the data which is the frontmatter
+    const {data} = parsedContent
+    return {
+      slug,
+      data,
+    };
+  });
+    // sort posts by oldest to newest
+    const sortedPosts = posts.sort(
+      (firstEl, secondEl) => secondEl.data.date - firstEl.data.date
+    ).reverse();
   
-//   return {
-//     props:{
-//       posts: sortedPosts
-//     }
-//   }
-// }
+  return {
+    props:{
+      posts: sortedPosts
+    }
+  }
+}
