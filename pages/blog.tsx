849ccb3c88c02e53180aad22dfe9d3651b4fee3c
André Hatlo-Johansen <ahatloj@gmail.com>
2023-01-21 20:00:02 +0100

    clean up

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from 'react';
 import fs from 'fs'
 import matter from "gray-matter";
-// import BlogCard from '../components/BlogCard';
 import Card from '../components/Card';
-
-
-
 
 interface BlogProps {
   posts: {
@@ -36,7 +32,7 @@ const Blog: React.FC<BlogProps> = ({posts}) => {
     <div className="justify-center w-3/4 mx-auto align-middle w-100 ">
       <div className="grid gap-2 mt-12 sm:grid-cols-2 lg:grid-cols-3">
         {posts.map(( post, index) => (
-          // <BlogCard key={index} post={post} />
+
           <Card key={index} post={post} />
         ))}
       </div>
@@ -45,32 +41,32 @@ const Blog: React.FC<BlogProps> = ({posts}) => {
 }
 export default Blog;
 
-export async function getStaticProps(){
-  // Getting all our posts at build time
-  // Get all the posts from posts folder
-  const files = fs.readdirSync("posts");
-  // Loop over each post to extract the frontmatter which we need
-  const posts = files.map((file) => {
-    // getting the slug here which we will need as a URL query parameter
-    const slug = file.replace(".md", "");
-    // Reading the contents of the file
-    const filecontent = fs.readFileSync(`posts/${file}`, "utf-8");
-    const parsedContent = matter(filecontent);
-    //The parsed content contains data and content we only need the data which is the frontmatter
-    const {data} = parsedContent
-    return {
-      slug,
-      data,
-    };
-  });
-    // sort posts by oldest to newest
-    const sortedPosts = posts.sort(
-      (firstEl, secondEl) => secondEl.data.date - firstEl.data.date
-    ).reverse();
+// export async function getStaticProps(){
+//   // Getting all our posts at build time
+//   // Get all the posts from posts folder
+//   const files = fs.readdirSync("posts");
+//   // Loop over each post to extract the frontmatter which we need
+//   const posts = files.map((file) => {
+//     // getting the slug here which we will need as a URL query parameter
+//     const slug = file.replace(".md", "");
+//     // Reading the contents of the file
+//     const filecontent = fs.readFileSync(`posts/${file}`, "utf-8");
+//     const parsedContent = matter(filecontent);
+//     //The parsed content contains data and content we only need the data which is the frontmatter
+//     const {data} = parsedContent
+//     return {
+//       slug,
+//       data,
+//     };
+//   });
+//     // sort posts by oldest to newest
+//     const sortedPosts = posts.sort(
+//       (firstEl, secondEl) => secondEl.data.date - firstEl.data.date
+//     ).reverse();
   
-  return {
-    props:{
-      posts: sortedPosts
-    }
-  }
-}
+//   return {
+//     props:{
+//       posts: sortedPosts
+//     }
+//   }
+// }
