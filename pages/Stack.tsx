11ef60104b3760cf7d8b8f4a80c937efe9a2d759
André Hatlo-Johansen <andrehatlo@icloud.com>
2023-01-24 13:51:46 +0100

    stack

--- pages/Stack.tsx
+++ pages/Stack.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Skills from "../components/Skills";
 
-interface StackProps {
-}
-
-const Stack: React.FC<StackProps> = () => {
+const Stack: NextPage = () => {
     return (            
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
